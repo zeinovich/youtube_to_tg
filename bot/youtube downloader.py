d710eeabd77dcff507cc668dbb8ff3af704e7bce
zeinovich <nikita.zinovich@gmail.com>
2022-10-18 21:01:35 +0300

    Added .gitignore - deleted config.py - instead created .env - added .env to .gitignore - slight changes to youtube downloader

--- bot/youtube downloader.py
+++ bot/youtube downloader.py
@@ -2,7 +2,6 @@ import os, sys
 from pathlib import Path
 from datetime import datetime
 import requests
-from config import path
 from cursor_module import Cursor
 from pytube import YouTube, Playlist
 import psycopg2
@@ -21,13 +20,13 @@ def to_database(video_id) -> None:
         you_video = YouTube(link)
 
     except Exception:
-        print(f"[ERROR] Link #{video_ids.index(video_id) + 1} has an error in it")
+        print(f"[ERROR] {datetime.now()} -> Link #{video_ids.index(video_id) + 1} has an error in it")
 
     video_id = you_video.video_id
     check_exists = curs.check_exists('videos', video_id)
     if not check_exists:
         author = you_video.author
-        title = ''.join(ch for ch in you_video.title if ch.isalnum() or ch in [' ','-',',',';',':','#'])
+        title = you_video.title
         duration = you_video.length
         curs.insert(columns=['video_id', 'author', 'title', 'duration'], 
                     values=[f"'{video_id}'", f"'{author}'", f"'{title}'", f"'{duration}'"], 
@@ -39,64 +38,68 @@ def download_link(video_id) -> None:
         you_video = YouTube(link)
 
     except Exception:
-        print(f"[ERROR] Link #{video_ids.index(link) + 1} has an error in it")
+        print(f"[ERROR] {datetime.now()} -> Link #{video_ids.index(link) + 1} has an error in it")
 
     title = curs.select(columns=['title'],
                         table='videos',
                         where=f"video_id = '{video_id}'")[0][0]
 
-    print(f'[INFO] Downloading: {title}')
-    you_download = you_video.streams.filter(only_audio=True).first()
+    print(f'[INFO] {datetime.now()} -> Downloading: {title}')
+    bot.send_message(chat_id, f'Downloading: {title}')
+    you_download = you_video.streams.filter(only_audio=True, audio_codec="opus").first()
     f_size = you_download.filesize/1024/1024
-    print(f'[SIZE] {f_size:.0f} Mb')
+    print(f'[INFO] {datetime.now()} -> {f_size:.0f} Mb')
     try:
-        you_download.download(f'{path}/audio', filename=f'{title}.mp3')
-        audio_path = f'{path}/audio/{title}.mp3'
-        thumb = you_video.thumbnail_url
-        download_thumb(thumb, video_id)
+        you_download.download(f'{PATH}/audio', filename=f'{video_id}.mp3')
+        download_thumb(video_id)
         sys.stdout.write("\033[K")
-        print('[INFO] Downloaded ')
-        curs.update(table='videos', 
-                    set=f"audio_path = '{audio_path}', thumb = '{thumb}'", 
-                    where=f"video_id = '{video_id}'")
+        print(f'[INFO] {datetime.now()} -> Downloaded ')
     except Exception as e:
-        print(f'[ERROR] Exception raised while downloading {title}\n {e}')
+        print(f'[ERROR] {datetime.now()} -> Exception raised while downloading {title}\n {e}')
 
 
-def download_thumb(url, video_id) -> None:
+def download_thumb(video_id) -> None:
+    url = f'https://i.ytimg.com/vi/{video_id}/sddefault.jpg'
     thumb = requests.get(url, allow_redirects=True)
-    f = f'{path}/thumbnails/{video_id}.png'
+    f = f'{PATH}/thumbnails/{video_id}.png'
 
     with open(f, 'wb') as file:
         file.write(thumb.content)
 
-    curs.update(table='videos', 
-                set=f"thumb = '{f}'", 
-                where=f"video_id = '{video_id}'")
-
-def tg_upload(video_id, chat_id) -> None:
-    print('[INFO] Uploading to telegram')
-
-    title, author, duration, thumb, audio_path = curs.select(
-    ['title, author, duration, thumb, audio_path'], 
+def tg_upload(video_id) -> None:
+    print(f'[INFO] {datetime.now()} -> Uploading to telegram')
+    audio_path = f'{PATH}/audio/{video_id}.mp3'
+    thumb_path = f'{PATH}/thumbnails/{video_id}.png'
+    title, author, duration = curs.select(
+    ['title', 'author', 'duration'], 
     table='videos', where=f"video_id = '{video_id}'")[0]
+    print(f'[INFO] {datetime.now()} -> Uploading: {title}')
+    bot.send_message(chat_id, f'Uploading: {title}')
+    with open(audio_path, 'rb') as audio, open(thumb_path, 'rb') as thumb:
+        bot.send_audio(chat_id=chat_id, audio=audio, title=title, 
+                        performer=author, duration=duration, 
+                        thumb=thumb)
 
-    bot.send_audio(chat_id=chat_id, audio = open(audio_path, 'rb'), performer=author, title=title, duration=duration, thumb=thumb)
     curs.insert(columns=['video_id', 'chat_id', 'date'], 
                 values=[f"'{video_id}'", f"'{chat_id}'", f"'{datetime.now()}'"], 
                 table='users')
-    print('[INFO] Uploaded')
+    print(f'[INFO] {datetime.now()} -> Uploaded')
 
 def main():
-    os.chdir(f'{path}')
+    global PATH
+    PATH = os.getenv('PATH')
+    API_KEY = os.getenv('API_KEY')
+    PASSWORD = os.getenv('PASSWORD')
+
+    os.chdir(PATH)
     Path('audio').mkdir(exist_ok=True)
     Path('thumbnails').mkdir(exist_ok=True)
 
     global bot
-    api_key = '##'
+    api_key = API_KEY
     bot = telebot.TeleBot(api_key)
 
-    conn = psycopg2.connect(database='youtube', user='postgres', password='##', host='localhost', port='5432')
+    conn = psycopg2.connect(database='youtube', user='postgres', password=PASSWORD, host='localhost', port='5432')
     conn.autocommit = True
     global curs
     curs = Cursor(conn)
@@ -112,26 +115,23 @@ def main():
             else:
                 bot.reply_to(message, 'Invalid link')
                 return
+            
+            global chat_id
+            chat_id = message.chat.id
 
             global video_ids
             video_ids = [link.split('watch?v=')[1] for link in links]
             
             # video_id, title, author, description, duration, thumb, audio_path
             for video_id in video_ids:
-                audio_path = curs.select(['audio_path'], 'videos', f"video_id = '{video_id}'")[0]
-                print(audio_path)
-                if not audio_path:
-                    to_database(video_id)
-                    download_link(video_id)
-                
-                chat_id = message.chat.id
-                bot.send_message(chat_id, 'Uploading...')
-                tg_upload(video_id, chat_id)
+                to_database(video_id)
+                download_link(video_id)
+                tg_upload(video_id)
 
         bot.polling(none_stop=True)
         
     except KeyboardInterrupt:
-        print('[STOP] KeyboardInterrupt')
+        print(f'[STOP] {datetime.now()} -> KeyboardInterrupt')
         curs.close()
         conn.close()
     
