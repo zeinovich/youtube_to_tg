06e70d0a8771bc3412814ebbf12d697b0e172a4a
zeinovich <nikita.zinovich@gmail.com>
2022-10-23 10:46:25 +0300

    Slight debugging - fixed bugs on SQL - more info to terminal

--- bot/video.py
+++ bot/video.py
@@ -4,7 +4,7 @@ from telebot import TeleBot
 import os, requests
 from datetime import datetime
 
-PATH = 'C:/Users/nikit/youtube_to_tg'
+PATH = os.getcwd()
 class Video(YouTube):
     def __init__(self, video_id, only_audio=True):
         super().__init__(f'https://www.youtube.com/watch?v={video_id}')
@@ -26,19 +26,22 @@ class Video(YouTube):
             if self.only_audio:
                 print(f'[INFO] {datetime.now()} -> Downloading: {self.title}')
                 au = self.streams.filter(only_audio=True).first()
-                print(f'[INFO] Size = {au.filesize / (1024 ** 2)} MB')
-                au.download(output_path=f'{PATH}/audios', filename=f'{self.video_id}.mp3')
+                print(f'[INFO] {datetime.now()} -> Size = {(au.filesize / (1024 ** 2)):.1f} MB')
+                au.download(output_path=f'{PATH}/audios', filename=f'{self.video_id}.mp3',
+                            skip_existing=True)
             else:
                 vid = self.streams.filter(res='1080p').first()
-                print(f'[INFO] Size = {vid.filesize / (1024 ** 2)} MB')
+                print(f'[INFO] {datetime.now()} -> Size = {(vid.filesize / (1024 ** 2)):.1f} MB')
                 vid.download(output_path=f'{PATH}/videos', filename=f'{self.video_id}.mp4')
+            print(f'[INFO] {datetime.now()} -> Downloading is finished')
         except Exception as e:
             print(e)
             
     def download_thumb(self):
         print(f'[INFO] {datetime.now()} -> Downloading thumbnail')
-        with open(f'{PATH}/thumbnails/{self.video_id}.png', 'w') as f:
-            f.write(requests.get(self.thumbnail_url, allow_redirects=True).content)
+        with open(f'{PATH}/thumbnails/{self.video_id}.png', 'wb') as f:
+            r = requests.get(self.thumbnail_url, allow_redirects=True)
+            f.write(r.content)
     
     def upload(self, bot: TeleBot, chat_id: int, curs: Cursor, file_id: str):
         thumb_path = f'{PATH}/thumbnails/{self.video_id}.png'
@@ -54,6 +57,7 @@ class Video(YouTube):
                 bot.send_video(chat_id=chat_id, video=file_id, 
                                 title=self.title, duration=self.duration, 
                                 performer=self.author, thumb=thumb)
+        print(f'[INFO] {datetime.now()} -> Uploading is finished')
         
         curs.insert(columns=['video_id', 'chat_id', 'date', 'only_audio'], 
                     values=[f"'{self.video_id}'", f"'{chat_id}'", f"'{datetime.now()}'", f"'{self.only_audio}'"], 
@@ -77,9 +81,10 @@ class Video(YouTube):
                     bot.send_video(chat_id=chat_id, video=video, 
                                     title=self.title, duration=self.duration, 
                                     performer=self.author, thumb=thumb)
+        print(f'[INFO] {datetime.now()} -> Uploading is finished')
         
         curs.insert(columns=['video_id', 'chat_id', 'date', 'only_audio'], 
-                    values=[f"'{self.video_id}'", f"'{chat_id}'", f"'{datetime.now()}'"], 
+                    values=[f"'{self.video_id}'", f"'{chat_id}'", f"'{datetime.now()}'", f"'{self.only_audio}'"], 
                     table='users')
 
     def __repr__(self):
